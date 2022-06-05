b7403a616988bf2c2e3cb6f44a460a6662626d62
J-a-yy <mvk1407@gmail.com>
2022-06-05 16:55:52 +0530

    configured and tested HTTP response

--- nodejsapi/src/index.js
+++ nodejsapi/src/index.js
@@ -1,19 +1,51 @@
+import dotenv from 'dotenv'
+import express from 'express'
+import ip from 'ip'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import Response from './domain/response.js'
 
-import express from 'express';
-import ip from 'ip';
-import dotenv from 'dotenv';
-import cors from 'cors';
 
-dotenv.config();
-const PORT = process.env.SERVER_PORT || 3000;
-const app = express();
-app.use(cors({origin: '*'}));
-app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send({message: 'UP'})
-});
 
-console.log(process.env);
+// const HttpStatus = {
+//     OK: { code: 200, status: 'OK' },
+//     CREATED: { code: 201, status: 'CREATED' },
+//     NO_CONTENT: { code: 204, status: 'NO_CONTENT' },
+//     BAD_REQUEST: { code: 400, status: 'BAD_REQUEST' },
+//     NOT_FOUND: { code: 404, status: 'NOT_FOUND' },
+//     INTERNAL_SERVER_ERROR: { code: 500, status: 'INTERNAL_SERVER_ERROR' },
+//   }
 
-app.listen(PORT, ()=>console.log(`server is running on ${ip.address()}: ${PORT}`));
+
+
+dotenv.config()
+const PORT = process.env.SERVER_PORT || 3000
+const app = express()
+
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(cors({ origin: '*' })) // not recommended
+
+// console.log(HttpStatus.OK.code)
+
+app.get('/', (req, res) =>
+  res.send(
+    new Response(
+      200,
+      'OK',
+      `Patient API v ${process.env.npm_package_version} \n All Systems GOOD`,
+      { patients: {name:'Junior' }}
+
+    )
+  )
+)
+
+
+
+// console.log(process.env)
+
+app.listen(PORT, () =>
+  console.log(`server running on: http://${ip.address()}:${PORT}`)
+)
