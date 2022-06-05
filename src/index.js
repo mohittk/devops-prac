29de32424b406a3c9e046e1370724346720dc812
J-a-yy <mvk1407@gmail.com>
2022-06-05 17:44:09 +0530

    added controller, utils, query folder

--- src/index.js
+++ src/index.js
@@ -4,19 +4,8 @@ import ip from 'ip'
 import cors from 'cors'
 import bodyParser from 'body-parser'
 import Response from './domain/response.js'
-
-
-
-
-// const HttpStatus = {
-//     OK: { code: 200, status: 'OK' },
-//     CREATED: { code: 201, status: 'CREATED' },
-//     NO_CONTENT: { code: 204, status: 'NO_CONTENT' },
-//     BAD_REQUEST: { code: 400, status: 'BAD_REQUEST' },
-//     NOT_FOUND: { code: 404, status: 'NOT_FOUND' },
-//     INTERNAL_SERVER_ERROR: { code: 500, status: 'INTERNAL_SERVER_ERROR' },
-//   }
-
+import logger from './utils/logger.js'
+import HttpStatus from './controller/patient.controller.js'
 
 
 dotenv.config()
@@ -33,15 +22,13 @@ app.use(cors({ origin: '*' })) // not recommended
 app.get('/', (req, res) =>
   res.send(
     new Response(
-      200,
-      'OK',
-      `Patient API v ${process.env.npm_package_version} \n All Systems GOOD`,
-      { patients: {name:'Junior' }}
-
+      HttpStatus.OK.code, HttpStatus.OK.status,
+      'Patient API - all systems good '
     )
   )
 )
 
 app.listen(PORT, () =>
-  console.log(`server running on: http://${ip.address()}:${PORT}`)
-)
+  // console.log(`server running on: http://${ip.address()}:${PORT}`)
+  logger.info(`server running on: http://${ip.address()}:${PORT}`)
+);
