45ff4f6c6856d9528be633f04a7cc5a1dfd77cde
J-a-yy <mvk1407@gmail.com>
2022-06-06 17:44:06 +0530

    declared routes

---
+++ src/route/patient.route.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import {getPatients, createPatient, getPatient, deletePatient, updatePatient} from '../controller/patient.controller';
+
+const patientRoutes = express.Router();
+
+patientRoutes.route('/')
+.get(getPatients)
+.post(createPatient);
+
+patientRoutes.route('/:id')
+.get(getPatient)
+.put(updatePatient)
+.delete(deletePatient);
